317ef7716a30a6f4c96a80ce56b2d7100a31e9fb
TANMAY KUDKAR <kudkartanmay25@gmail.com>
2025-09-14 23:24:50 +0530

    Add frontend and backend as a single repository

---
+++ mycollegemart-backend/src/test/java/com/mycollegemart/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycollegemart.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
